8688a7ad33668a3ce8a4bc9eec9de35fdcf195f3
Peizhen Yang <83687784+PeizhenYang@users.noreply.github.com>
2023-04-18 20:41:33 +0800

    Update syn_test_1.py

--- syn_test_1.py
+++ syn_test_1.py
@@ -6,16 +6,18 @@ from torch.utils.data import DataLoader, TensorDataset
 
 
 class FeatureExtractor(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=4, stride=1, padding=1):
+    def __init__(self, in_channels, out_channels, kernel_size=(3,3), stride=1, padding=1):
         super(FeatureExtractor, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
+        print(x.shape)
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
+        print(x.shape)
         return x
 
 
@@ -27,14 +29,17 @@ class SpatialSelfAttention(nn.Module):
         self.softmax_layer = nn.Softmax(dim=-1)
 
     def forward(self, H):
-        N, T = H.size()
-        Q = self.query_layer(H)
-        K = self.key_layer(H)
-
-        A = torch.matmul(Q, K.transpose(-1, -2))
-        A = torch.where(torch.eye(N, device=H.device), torch.zeros_like(A), A)
+        N, T = H.shape[0],H.shape[1]
+        print(H.shape)
+        Q = self.query_layer(H.transpose(-1,-2))
+        K = self.key_layer(H.transpose(-1,-2))
+        print(Q.size(),K.size())
+        A = torch.matmul(Q.transpose(-1, -2), K)
+        #A = torch.where(torch.eye(N, device=H.device,dtype=torch.bool), torch.zeros_like(A), A)
         A /= torch.sqrt(torch.tensor(K.size(-1)).float())
+        print(A.size())
         A = self.softmax_layer(A)
+        print(A.size(),H.size())
         H_ = torch.matmul(A, H)
         return H_ + H
 
@@ -56,7 +61,7 @@ class TemporalWeightedSum(nn.Module):
 class EncoderLayer(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_heads):
         super(EncoderLayer, self).__init__()
-        self.attention_layer = nn.MultiheadAttention(hidden_dim, num_heads)
+        self.attention_layer = SpatialSelfAttention(hidden_dim, hidden_dim)
         self.extractor_layer = FeatureExtractor(input_dim, hidden_dim)
         self.norm1 = nn.LayerNorm(hidden_dim)
         self.norm2 = nn.LayerNorm(hidden_dim)
@@ -65,20 +70,21 @@ class EncoderLayer(nn.Module):
         print(f'Encoder Layer Input: {x.shape}')
         out = self.extractor_layer(x).transpose(1, 2)
         print(f'Encoder Layer Extractor Output: {out.shape}')
-        attn_output, _ = self.attention_layer(out, out, out)
+        attn_output = self.attention_layer(out)
         print(f'Encoder Layer Attention Output: {attn_output.shape}')
-        out = (out + attn_output).permute(1, 2, 0)
+        print(out.shape,attn_output.shape)
+        out = (out + attn_output).permute(1, 0,3,2)
         out = self.norm1(out)
-        out2 = self.norm2(out + self.extractor_layer(out.transpose(1, 2)).transpose(1, 2))
+        out2 = self.norm2(out + self.extractor_layer(out.permute(1,3,0,2)).transpose(1, 2))
         return out2
 
 
 class DecoderLayer(nn.Module):
     def __init__(self, hidden_dim, num_heads):
         super(DecoderLayer, self).__init__()
-        self.self_attention_layer = nn.MultiheadAttention(hidden_dim, num_heads)
+        self.self_attention_layer = SpatialSelfAttention(hidden_dim, num_heads)
         self.norm1 = nn.LayerNorm(hidden_dim)
-        self.attention_layer = nn.MultiheadAttention(hidden_dim, num_heads)
+        self.attention_layer = SpatialSelfAttention(hidden_dim, num_heads)
         self.norm2 = nn.LayerNorm(hidden_dim)
         self.norm3 = nn.LayerNorm(hidden_dim)
 
@@ -104,9 +110,11 @@ class Transformer(nn.Module):
         self.fc_layer = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
-        encoder_out = x.transpose(0, 1)
+        encoder_out = x.transpose(0,1)
         for layer in self.encoder_layers:
-            encoder_out = layer(encoder_out)
+            print('1',encoder_out.shape)
+            encoder_out = layer(encoder_out.transpose(1,2))
+            print('1',encoder_out.shape)
         decoder_out = encoder_out
         for layer in self.decoder_layers:
             decoder_out = layer(decoder_out, encoder_out)
@@ -116,20 +124,27 @@ class Transformer(nn.Module):
 
 
 seq_len = 10
-batch_size = 4
-input_dim = 32
+batch_size = 1
+input_dim = 64
 hidden_dim = 64
 num_layers = 2
 num_heads = 4
 
-rnn = nn.RNN(input_size=input_dim,hidden_size=hidden_dim,num_layers=num_layers)
-x = torch.randn(seq_len, batch_size, input_dim)
-h_0 = torch.zeros(num_layers,batch_size,hidden_dim)
-out, h = rnn(x, h_0)
+x = torch.randn(input_dim,1,seq_len,batch_size)
+
+
+in_channels = 10
+out_channels = 64
+
+
+#print(output_fe)
+
+
+# ws = SpatialSelfAttention(input_dim, hidden_dim)
+# output_ws = ws(x)
+# print(output_sa)
+
+
 model = Transformer(input_dim, hidden_dim, num_layers, num_heads)
-output = model(h)
-# print(output)
-print("out.shape:",out.shape)
-print("h.shape:",h.shape)
-# torch.Size([10, 4, 32])
-# torch.Size([4, 1])
+output = model(x)
+print(f'Output shape: {output.shape}')
